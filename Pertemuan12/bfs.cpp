f66329f6b9a9ce60e9532beba41432820911134c
ranggaaldosas <ranggaaldosas@gmail.com>
2022-11-16 10:37:50 +0700

    add pertemuan12

--- Pertemuan12/bfs.cpp
+++ Pertemuan12/bfs.cpp
@@ -1,60 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <queue>
 using namespace std;
 
-// This class represents a directed graph using
-// adjacency list representation
 class Graph
 {
-    int V; // No. of vertices
-
-    // Pointer to an array containing adjacency
-    // lists
+    int v;
     vector<list<int>> adj;
 
 public:
-    Graph(int V); // Constructor
-
-    // function to add an edge to graph
+    Graph(int v);
     void addEdge(int v, int w);
-
-    // prints BFS traversal from a given source s
     void BFS(int s);
 };
-
-Graph::Graph(int V)
+Graph::Graph(int v)
 {
-    this->V = V;
-    adj.resize(V);
+    this->v = v;
+    adj.resize(v);
 }
-
 void Graph::addEdge(int v, int w)
 {
-    adj[v].push_back(w); // Add w to v’s list.
+    adj[v].push_back(w);
 }
-
 void Graph::BFS(int s)
 {
-    // Mark all the vertices as not visited
     vector<bool> visited;
-    visited.resize(V, false);
-
-    // Create a queue for BFS
+    visited.resize(v, false);
     list<int> queue;
-
-    // Mark the current node as visited and enqueue it
     visited[s] = true;
     queue.push_back(s);
-
     while (!queue.empty())
     {
-        // Dequeue a vertex from queue and print it
         s = queue.front();
         cout << s << " ";
         queue.pop_front();
-
-        // Get all adjacent vertices of the dequeued
-        // vertex s. If a adjacent has not been visited,
-        // then mark it visited and enqueue it
         for (auto adjecent : adj[s])
         {
             if (!visited[adjecent])
@@ -66,21 +47,14 @@ void Graph::BFS(int s)
     }
 }
 
-// Driver program to test methods of graph class
 int main()
 {
-    // Create a graph given in the above diagram
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
+    Graph g(5);
+    g.addEdge(4, 2);
+    g.addEdge(4, 3);
+    g.addEdge(2, 1);
     g.addEdge(2, 3);
-    g.addEdge(3, 3);
-
-    cout << "Following is Breadth First Traversal "
-         << "(starting from vertex 2) \n";
+    g.addEdge(1, 3);
+    g.addEdge(0, 1);
     g.BFS(4);
-
-    return 0;
 }
